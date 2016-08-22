4ec315480c50c11d06174a5d3ab2913013138ff3
Kyle Mathews <mathews.kyle@gmail.com>
2016-08-22 10:29:43 -0700

    Update site

--- www/utils/typography.js
+++ www/utils/typography.js
@@ -1,5 +1,5 @@
 import Typography from 'typography'
-import theme from 'typography-theme-noriega'
+import theme from 'typography-theme-lawton'
 import CodePlugin from 'typography-plugin-code'
 theme.plugins = [new CodePlugin()]
 
@@ -7,6 +7,8 @@ const typography = new Typography(theme)
 
 // Hot reload typography in development.
 if (process.env.NODE_ENV !== 'production') {
+  const designTools = require('typography-design-tools')
+  designTools(typography)
   typography.injectStyles()
 }
 
